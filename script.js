2eb3fc4e6a21ad6d30464be7dc2451725c34b2ec
almightysophie <77300753+almightysophie@users.noreply.github.com>
2021-02-02 15:51:27 -0500

    ,

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-let img;
+let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
 
-function setup() {
-  createCanvas(300, 300);
-  img = loadImage('https://happycoding.io/images/stanley-1.jpg');
-}
-
-function draw() {
-  image(img, 0, 0);
-
-  // Get the color at the mouse position
-  let c = img.get(mouseX, mouseY);
-
-  // Change the fill to that color
-  fill(c);
-
-  // Draw a square with that color
-  square(mouseX, mouseY, 50);
-}
+$.getJSON(url, function(data){
+    console.log(data.facet[0].type_of_material);
+ })
+ 
